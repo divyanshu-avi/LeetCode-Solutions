9701e24d62ae8ecf8f8ea96c11feb8a0a5c05cfc
Divyanshu Singh <divyanshu.avi@gmail.com>
2020-12-31 22:37:08 +0530

    Made 29 a little better.

--- 029/29_d.cpp
+++ 029/29_d.cpp
@@ -5,17 +5,17 @@ public:
             return dividend;
         if(dividend == INT_MIN && divisor == -1)
             return INT_MAX;
-        int ans = 0, x, sign = (dividend>=0)==(divisor>=0);
+        int ans = 0, x, i, sign = (dividend>=0)==(divisor>=0);
         long long int a, b;
         a = abs(dividend);
         b = abs(divisor);
-        while(a-b >= 0)
+        for(i=31;i>=0;i--)
         {
-            x=0;
-            while(a - (b<<1<<x) >=0)
-                x++;
-            ans += 1<<x;
-            a -= b<<x;
+            if(b<<i <= a)
+            {
+                a -= b<<i;
+                ans += 1<<i;
+            }
         }
         return sign?ans:-ans;
     }
