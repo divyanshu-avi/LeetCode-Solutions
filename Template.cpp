9f84d983660f55f78f27a677549fc553b43abaed
Divyanshu Singh <divyanshu.avi@gmail.com>
2021-01-03 23:09:48 +0530

    Added c++ template.

---
+++ Template.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve();
+int main()
+{
+   ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+   #ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("error.txt", "w", stderr);
+   freopen("output.txt", "w", stdout);
+   #endif
+
+   int t;
+   cin>>t;
+   while(t--)
+   {
+      solve();
+      cout<<"\n";
+   }
+
+   cerr<<"time taken : "<<(float)clock()/CLOCKS_PER_SEC<<" secs"<<endl;
+   return 0;
+}
+void solve()
+{
+   
+}
