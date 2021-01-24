a1a89bf8b28f705860e70f3a9bc9b6582bbea6e3
Yash Agrawal <yash.agrawal0305@gmail.com>
2021-01-24 23:17:25 +0530

    Update 322_bottom_up.cpp

--- 322/322_bottom_up.cpp
+++ 322/322_bottom_up.cpp
@@ -1,41 +1,23 @@
 class Solution {
 public:
-    
     int coinChange(vector<int>& coins, int amount) {
         
         sort(coins.begin(), coins.end());
-        vector<int> a(amount+1,0);
-        return solve(coins,amount,a);
         
-    }
-    
-    int solve(vector<int>& coins, int target, vector<int>&a){
+        vector<int> a(amount+1,INT_MAX);
         
-        if(target==0)
-            return 0;
-        if(target<0)
-            return -1;
-        if(a[target]!= 0)
-            return a[target];
+        a[0] = 0;
         
-        int mini = INT_MAX;
-        int temp;
-        
-        for(int j =0; (j < coins.size() && target-coins[j]>=0); j++){
-            
-            temp = solve(coins,target - coins[j],a);
-              if(temp != -1)  
-            mini = min(mini, 1+ temp );
-            
+        for(int i =1; i<a.size() ; i++){   
+            for(int j =0; j<coins.size() && (i-coins[j])>=0 ; j++){
+                
+                if(a[i-coins[j]]!=INT_MAX)
+                a[i] = min(a[i],1+a[i-coins[j]] );
+              
+            }
+            cout<<a[i]<<endl;
         }
         
-        if(mini!= INT_MAX)
-        a[target] = mini;
-        else
-        a[target] = -1;
-        
-        return a[target];
-        
+        return a[amount]==INT_MAX ? -1:a[amount];
     }
-    
 };
